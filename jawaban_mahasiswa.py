a08fbf9d06db4fdf43418d91b96fd4fc92757881
mahasiswa4 <ican88030@gmail.com>
2026-05-15 17:41:53 +0700

    Update jawaban_mahasiswa.py

--- jawaban_mahasiswa.py
+++ jawaban_mahasiswa.py
@@ -1,1 +1,26 @@
-# JAWAB DISINI!
+def validasi_password(password):
+    if len(password) >= 8:
+        if len(password) <= 20:
+            if " " not in password:
+                has_upper = False
+                for char in password:
+                    if char >= 'A':
+                        if char <= 'Z':
+                            has_upper = True
+                            
+                if has_upper:
+                    has_digit = False
+                    for char in password:
+                        if char >= '0':
+                            if char <= '9':
+                                has_digit = True
+                                
+                    if has_digit:
+                        has_symbol = False
+                        for char in password:
+                            if char in "!@#$%^&*":
+                                has_symbol = True
+                                
+                        if has_symbol:
+                            return True
+    return False
